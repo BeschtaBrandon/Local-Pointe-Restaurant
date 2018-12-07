2833144711cd729b092abb6262c44e2ba8a677c3
ASI1\bbeschta <brandon.beschta@wtsparadigm.com>
2018-12-07 11:25:12 -0600

    Testing out heroku

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -37,7 +37,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Lyndon Station, population 500 ! Make it 501 and move in today!</h2>
         </div>
         <p className="App-intro">
           {'This is '}
