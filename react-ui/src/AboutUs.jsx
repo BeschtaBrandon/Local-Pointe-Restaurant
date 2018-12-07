a2e67216eb4fd239ffe0db205299f14e33334347
ASI1\bbeschta <brandon.beschta@wtsparadigm.com>
2018-12-07 15:34:26 -0600

    Add some more details

--- react-ui/src/AboutUs.jsx
+++ react-ui/src/AboutUs.jsx
@@ -13,10 +13,10 @@ class AboutUs extends React.Component {
         </div>
         <main>
         <div className='col-6 section-about-us'>
-          <div className='about-us'><h4><a href='#'>About Us</a></h4><p>Rated over 4 stars <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> on Google Reviews!.</p><p className='read-more'><a>Read more...</a></p></div>
-          <div id='menu' className='about-us'><h4><a href='#'>Our Menu</a></h4><p>Nulla imperdiet sit amet magna. Vestibulum dapibus, mauris nec malesuada fames ac turpis velit, rhoncus eu, luctus et interdum adipiscing wisi. Aliquam sem. In hendrerit nulla quam nunc, accumsan congue.</p><p className='read-more'><a>read more...</a></p></div>
-          <div className='about-us about-us_recipes'><div className='about-us_photo'></div><div className='about-us_recipes-info'><h4><a href='#'>Our Recipes</a></h4><p>Ea dicit iudico persecuti qui, in debitis disputationi sed, regione omnesque eos in. Eu nec aeque partem deserunt, ad vel nullam numquam.</p></div></div>
-          <div className='about-us'><h4><a href='#'>Location</a></h4><p>N1095 WI-16, Lyndon Station, WI 53944</p><p className='read-more'><a>read more...</a></p></div>
+          <div className='about-us'><h4><a href='#'>About Us</a></h4><p>Rated over 4 stars <i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i> on Google Reviews!.</p><p className='read-more'></p></div>
+          <div id='menu' className='about-us'><h4><a href='#'>Our Menu</a></h4><p>Home-made cuisine inspired by our local community.</p><p className='read-more'></p></div>
+          <div className='about-us about-us_recipes'><div className='about-us_photo'></div><div className='about-us_recipes-info'><h4><a href='#'>Our Food</a></h4><p>Never go home hungry when you visit the Local Pointe.</p></div></div>
+          <div className='about-us'><h4><a href='#'>Location</a></h4><p>N1095 WI-16, Lyndon Station, WI 53944</p><p className='read-more'></p></div>
         </div>
         </main>
       </div>
